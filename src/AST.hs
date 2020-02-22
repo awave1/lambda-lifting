c6c875c5c73e366347a22d86f1fadf0d182e6d5c
Artem Golovin <artgolovin96@gmail.com>
2020-02-22 02:02:12 -0700

    :recycle: clean up & reformat AST + provided "tests"

--- src/AST.hs
+++ src/AST.hs
@@ -1,10 +1,9 @@
 module AST where
 
 newtype Prog a b = Prog [Fun a b]
-
 newtype Fun a b = Fun (a, [b], Exp a b)
 
-data BoolExp a b   = Lt (Exp a b) (Exp a b)
+data BoolExp a b = Lt (Exp a b) (Exp a b)
                 | Gt (Exp a b) (Exp a b)
                 | Eq (Exp a b) (Exp a b)
                 | AND (BoolExp a b) (BoolExp a b)
@@ -22,41 +21,45 @@ data Exp a b = ADD (Exp a b) (Exp a b)
              | APP a [Exp a b]
              | LET [Fun a b] (Exp a b)
 
-
 -- Pretty Printer
 class Printer a where
     printer:: a -> String
 
 instance Printer a => Printer [a] where
-    printer []       = []
-    printer (a : as) = printer a ++ printer as
+  printer []       = []
+  printer (a : as) = printer a ++ printer as
 
 instance Printer Int where
-    printer n = "v" ++ show (n :: Int)
+  printer n = "v" ++ show (n :: Int)
 
 instance Printer Char where
-    printer c = [c]
+  printer c = [c]
 
--- to print n spaces
-spaces 0 = ""
-spaces n = " " ++ spaces (n - 1)
+
+-- to print n printSpaces
+printSpaces :: Int -> String
+printSpaces 0 = ""
+printSpaces n = " " ++ printSpaces (n - 1)
 
 showProg :: (Printer a, Printer b) => Prog a b -> String
-showProg (Prog funs) = concat (map (\f -> showFun 0 f ++ "\n")) funs
+-- showProg (Prog funs) = concat (map (\f -> (showFun 0 f) ++ "\n") funs)
+showProg (Prog funs) = concatMap (\f -> showFun 0 f ++ "\n") funs
 
 showFun :: (Printer a, Printer b) => Int -> Fun a b -> String
 showFun n (Fun (fname, a1 : args, body)) =
-    spaces n
-        ++ "fun "
-        ++ printer fname
-        ++ "("
-        ++ printer a1
-        ++ concatMap (\a -> "," ++ printer a) args
-        ++ ") = "
-        ++ showExp' n body
+  printSpaces n
+    ++ "fun "
+    ++ printer fname
+    ++ "("
+    ++ printer a1
+    ++ concatMap (\a -> "," ++ printer a) args
+    ++ ") = "
+    ++ showExp' n body
+showFun n (Fun (fname, [], body)) =
+  printSpaces n ++ printer fname ++ "() = " ++ showExp' n body
 
 showExp :: (Printer a, Printer b) => Int -> Exp a b -> String
-showExp n exp = spaces n ++ showExp' n exp
+showExp n exp = printSpaces n ++ showExp' n exp
 
 showExp' :: (Printer a, Printer b) => Int -> Exp a b -> String
 showExp' n (ADD e1 e2) = showExp' n e1 ++ "+" ++ showExp' n e2
@@ -67,32 +70,32 @@ showExp' n (NEG   e  ) = "-" ++ showExp' n e
 showExp' n (CONST m  ) = show m
 showExp' n (VAR   b  ) = printer b
 showExp' n (COND b e1 e2) =
-    "(if "
-        ++ showBoolExp n b
-        ++ "\\n"
-        ++ spaces (n + 3)
-        ++ "then "
-        ++ showExp' (n + 3) e1
-        ++ "\n"
-        ++ spaces (n + 3)
-        ++ "else "
-        ++ showExp' (n + 3) e1
-        ++ ")"
+  "(if "
+    ++ showBoolExp n b
+    ++ "\\n"
+    ++ printSpaces (n + 3)
+    ++ "then "
+    ++ showExp' (n + 3) e1
+    ++ "\n"
+    ++ printSpaces (n + 3)
+    ++ "else "
+    ++ showExp' (n + 3) e1
+    ++ ")"
 showExp' n (APP f (e : es)) =
-    printer f
-        ++ "("
-        ++ showExp' n e
-        ++ concatMap (\x -> "," ++ showExp' n x) es
-        ++ ")"
+  printer f
+    ++ "("
+    ++ showExp' n e
+    ++ concatMap (\x -> "," ++ showExp' n x) es
+    ++ ")"
 showExp' n (LET [] e) = showExp' n e
 showExp' n (LET (f : fs) e) =
-    "let\n"
-        ++ showFun Int Fun a b (n + 3) f
-        ++ concat map (\f -> "\n" ++ showFun (n + 3) f) fs
-        ++ "\n"
-        ++ spaces n
-        ++ "in  "
-        ++ showExp' n e
+  "let\n"
+    ++ showFun (n + 3) f
+    ++ concatMap (\f -> "\n" ++ showFun (n + 3) f) fs
+    ++ "\n"
+    ++ printSpaces n
+    ++ "in  "
+    ++ showExp' n e
 
 showBoolExp :: (Printer a, Printer b) => Int -> BoolExp a b -> String
 showBoolExp n (Lt  e1 e2) = showExp' n e1 ++ "<" ++ showExp' n e2
