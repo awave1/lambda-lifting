5a964bae63a74402203092c902ac85e9ba5a0f52
Artem Golovin <artgolovin96@gmail.com>
2020-02-22 02:27:30 -0700

    :bug: fix test typings and print both tests at the same time

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,42 +5,38 @@ where
 
 import           AST
 
-test1 :: IO ()
-test1 = putStr
-  (showProg
-    (Prog
-      [ Fun ("main", [], ADD (VAR "X") (VAR "Y"))
-      , Fun
-        ( "f"
-        , ["x"]
-        , LET
-          [ Fun ("g", ["y"], MUL (VAR "y") (VAR "x"))
-          , Fun ("h", ["x", "y"], DIV (VAR "x") (VAR "y"))
-          ]
-          (ADD (APP "g" [VAR "x"]) (APP "h" [VAR "x", CONST 7]))
-        )
-      ] :: (Prog String String)
-    )
+test1 = showProg
+  (Prog
+    [ Fun ("main", [], ADD (VAR "X") (VAR "Y"))
+    , Fun
+      ( "f"
+      , ["x"]
+      , LET
+        [ Fun ("g", ["y"], MUL (VAR "y") (VAR "x"))
+        , Fun ("h", ["x", "y"], DIV (VAR "x") (VAR "y"))
+        ]
+        (ADD (APP "g" [VAR "x"]) (APP "h" [VAR "x", CONST 7]))
+      )
+    ] :: (Prog String String)
   )
 
-test2 :: IO ()
-test2 = putStr
-  (showProg
-    (Prog
-      [ Fun ("main", [], ADD (VAR 1) (VAR 2))
-      , Fun
-        ( "f"
-        , [1]
-        , LET
-          [ Fun ("g", [2], MUL (VAR 2) (VAR 2))
-          , Fun ("h", [1, 2], DIV (VAR 1) (VAR 2))
-          ]
-          (ADD (APP "g" [VAR 1]) (APP "h" [VAR 1, CONST 7]))
-        )
-      ] :: (Prog String Int)
-    )
+
+test2 = showProg
+  (Prog
+    [ Fun ("main", [], ADD (VAR 1) (VAR 2))
+    , Fun
+      ( "f"
+      , [1]
+      , LET
+        [ Fun ("g", [2], MUL (VAR 2) (VAR 2))
+        , Fun ("h", [1, 2], DIV (VAR 1) (VAR 2))
+        ]
+        (ADD (APP "g" [VAR 1]) (APP "h" [VAR 1, CONST 7]))
+      )
+    ] :: (Prog String Int)
   )
+
 
 
 someFunc :: IO ()
-someFunc = test1 ++ "\n" ++ test2
+someFunc = putStrLn (test1 ++ "\n" ++ test2)
