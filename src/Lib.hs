230f86f89c0ee78b59259ff9ee6dd347a9e5c130
Artem Golovin <artgolovin96@gmail.com>
2020-02-23 17:16:44 -0700

    feat: 🎸 move tests to test/Spec.hs and rename main function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,41 +1,13 @@
 module Lib
-  ( someFunc
+  ( lambdaLift
   )
 where
 
 import           AST
+import           System.Environment
 
-test1 = showProgram
-  (Prog
-    [ Fun ("main", [], ADD (VAR "X") (VAR "Y"))                 -- main() = X+Y
-    , Fun                                                       -- fun f(x)
-      ( "f"
-      , ["x"]
-      , LET                                                     -- let definition
-        [ Fun ("g", ["y"], MUL (VAR "y") (VAR "x"))             -- g(y) = y*x
-        , Fun ("h", ["x", "y"], DIV (VAR "x") (VAR "y"))        -- h(x, y) = x/y
-        ]
-        (ADD (APP "g" [VAR "x"]) (APP "h" [VAR "x", CONST 7])) -- in part: g(x) + h(x, 7)
-      )
-    ] :: (Prog String String)
-  )
-
-test2 = showProgram
-  (Prog
-    [ Fun ("main", [], ADD (VAR 1) (VAR 2))
-    , Fun
-      ( "f"
-      , [1]
-      , LET
-        [ Fun ("g", [2], MUL (VAR 2) (VAR 2))
-        , Fun ("h", [1, 2], DIV (VAR 1) (VAR 2))
-        ]
-        (ADD (APP "g" [VAR 1]) (APP "h" [VAR 1, CONST 7]))
-      )
-    ] :: (Prog String Int)
-  )
-
-
-
-someFunc :: IO ()
-someFunc = putStrLn (test1 ++ "\n" ++ test2)
+lambdaLift :: IO ()
+lambdaLift = do
+  args <- getArgs
+  file <- readFile $ head args
+  putStrLn file
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,36 @@
+import           AST
+
+test1 :: String
+test1 = showProgram
+  (Prog
+    [ Fun ("main", [], ADD (VAR "X") (VAR "Y"))                 -- main() = X+Y
+    , Fun                                                       -- fun f(x)
+      ( "f"
+      , ["x"]
+      , LET                                                     -- let definition
+        [ Fun ("g", ["y"], MUL (VAR "y") (VAR "x"))             -- g(y) = y*x
+        , Fun ("h", ["x", "y"], DIV (VAR "x") (VAR "y"))        -- h(x, y) = x/y
+        ]
+        (ADD (APP "g" [VAR "x"]) (APP "h" [VAR "x", CONST 7])) -- in part: g(x) + h(x, 7)
+      )
+    ] :: (Prog String String)
+  )
+
+test2 :: String
+test2 = showProgram
+  (Prog
+    [ Fun ("main", [], ADD (VAR 1) (VAR 2))
+    , Fun
+      ( "f"
+      , [1]
+      , LET
+        [ Fun ("g", [2], MUL (VAR 2) (VAR 2))
+        , Fun ("h", [1, 2], DIV (VAR 1) (VAR 2))
+        ]
+        (ADD (APP "g" [VAR 1]) (APP "h" [VAR 1, CONST 7]))
+      )
+    ] :: (Prog String Int)
+  )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = putStrLn (test1 ++ "\n" ++ test2)
